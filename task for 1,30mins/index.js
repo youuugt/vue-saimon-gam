2e532a4d048aeb98195f829cd13675031153ea15
youuugt <152985806+youuugt@users.noreply.github.com>
2023-12-23 19:58:34 +0600

    Add files via upload

---
+++ task for 1,30mins/index.js
@@ -0,0 +1,8 @@
+// const tasks = [
+//     {task: "go work" },
+//     {task: "shoping" },
+//     {task: "go to the gym" },
+//     {task: "breakfast" },
+// ]
+// console.log();
+// Находим элементы на странице
